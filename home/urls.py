f62a37bc48b6344d4a272c40659635064463ae64
Noah Balauro <noah@zenlogic.us>
2016-01-28 12:54:18 -0800

    This closes #3, closes #4, and closes #5

--- home/urls.py
+++ home/urls.py
@@ -1,1 +1,9 @@
+from django.conf.urls import url
 
+urlpatterns = [
+    url(r'^about/', 'home.views.about', name='about'),
+    url(r'^strains/', 'home.views.strains', name='strains'),
+    url(r'^wheretobuy/', 'home.views.wheretobuy', name='wheretobuy'),
+    url(r'^news/', 'home.views.news', name='news'),
+    url(r'^$', 'home.views.index', name='home'),
+]
