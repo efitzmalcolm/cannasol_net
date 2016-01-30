266653b019596b53610313a7abf2f85d6404b9a2
Noah Balauro <noah@zenlogic.us>
2016-01-29 23:04:30 -0800

    qaSample Admin Terpene Result Inline

--- strains/admin.py
+++ strains/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 from strains.models import *
 
 
+class TerpeneResultInline(admin.TabularInline):
+    model = TerpeneResult
+
+
+class qaSampleAdmin(admin.ModelAdmin):
+    inlines = [
+        TerpeneResultInline,
+    ]
+
+
 admin.site.register(Strain)
 admin.site.register(Brand)
-admin.site.register(qaSample)
+admin.site.register(qaSample, qaSampleAdmin)
 admin.site.register(Terpene)
+admin.site.register(TerpeneResult)
