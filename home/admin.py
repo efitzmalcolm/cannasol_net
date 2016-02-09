6b9a5a99da81c13a118d2fb8f94e6af891481df3
Noah Balauro <noah@zenlogic.us>
2016-02-09 08:31:29 -0800

    Working on #25. Models and Admin roughdraft

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from home.models import *
 
-# Register your models here.
+
+class CarouselAdmin(admin.ModelAdmin):
+    fields = ('name', 'preview_image', 'image')
+    readonly_fields = ['preview_image']
+
+
+class BrandSectionAdmin(admin.ModelAdmin):
+    fields = ('title', 'intro')
+
+    inlines = [BrandItemInline]
+
+    def has_add_permission(self, request):
+        base_add_permission = super(BrandSectionAdmin, self).has_add_permission(request)
+        if base_add_permission:
+            return not BrandSection.objects.exists()
+
+
+class BrandItemInline(admins.TabulerInline):
+    model = BrandItem
+    extra = 0
+    fields = ('name', 'desc', 'preview_image', 'image')
+    readonly_fields = ['preview_image']
+
+
+class InfoSectionAdmin(admin.ModelAdmin):
+    fields = ('preview_image', 'image')
+    readonly_fields = ['preview_image']
+
+    def has_add_permission(self, request):
+        base_add_permission = super(InfoSectionAdmin, self).has_add_permission(request)
+        if base_add_permission:
+            return not InfoSection.objects.exists()
+
+
+class TeamMemberAdmin(admin.ModelAdmin):
+    fields = ('name', 'bio', 'email', 'preview_image', 'image')
+    readonly_fields = ['preview_image']
+
+
+class RetailerAdmin(admin.ModelAdmin):
+    fields = ('name', 'preview_image', 'image')
+    readonly_fields = ['preview_image']
+
+
+admin.site.register(CarouselImage, CarouselAdmin)
+admin.site.register(BrandSection, BrandSectionAdmin)
+admin.site.register(InfoSection, InfoSectionAdmin)
+admin.site.register(TeamMember, TeamMemberAdmin)
+admin.site.register(Retailer, RetailerAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,91 @@
+from django.utils.safestring import mark_safe
 from django.db import models
 
-# Create your models here.
+
+class CarouselImage(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField()
+
+    class Meta:
+        verbose_name = 'Carousel Image'
+        verbose_name_plural = 'Carousel Images'
+
+    def __str__(self):
+        return self.name
+
+    # Generates HTML Image preview for use at thumbnail on admin, etc...
+    def preview_image(self):
+        if self.image and hasattr(self.image, 'url'):
+            return mark_safe('<img height="263px" width="263px" src="%s" />' % self.image.url)
+
+
+class BrandSection(models.Model):
+    title = models.CharField(max_length=30)
+    intro = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class BrandItem(models.Model):
+    name = models.CharField(max_length=30)
+    desc = models.TextField()
+    image = models.ImageField()
+    section = models.ForeignKey('BrandSection')
+
+    class Meta:
+        verbose_name = 'Brand Item'
+        verbose_name_plural = 'Brand Items'
+
+    def __str__(self):
+        return self.name
+
+    # Generates HTML Image preview for use at thumbnail on admin, etc...
+    def preview_image(self):
+        if self.image and hasattr(self.image, 'url'):
+            return mark_safe('<img height="263px" width="263px" src="%s" />' % self.image.url)
+
+
+class InfoSection(models.Model):
+    image = models.ImageField()
+
+    # Generates HTML Image preview for use at thumbnail on admin, etc...
+    def preview_image(self):
+        if self.image and hasattr(self.image, 'url'):
+            return mark_safe('<img height="263px" width="263px" src="%s" />' % self.image.url)
+
+
+class TeamMember(models.Model):
+    name = models.CharField(max_length=30)
+    bio = models.TextField()
+    email = models.EmailField()
+    image = models.ImageField()
+
+    class Meta:
+        verbose_name = 'Team Member'
+        verbose_name_plural = 'Team Members'
+
+    def __str__(self):
+        return self.name
+
+    # Generates HTML Image preview for use at thumbnail on admin, etc...
+    def preview_image(self):
+        if self.image and hasattr(self.image, 'url'):
+            return mark_safe('<img height="263px" width="263px" src="%s" />' % self.image.url)
+
+
+class Retailer(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField()
+
+    class Meta:
+        verbose_name = 'Retailer'
+        verbose_name_plural = 'Retailers'
+
+    def __str__(self):
+        return self.name
+
+    # Generates HTML Image preview for use at thumbnail on admin, etc...
+    def preview_image(self):
+        if self.image and hasattr(self.image, 'url'):
+            return mark_safe('<img height="263px" width="263px" src="%s" />' % self.image.url)
