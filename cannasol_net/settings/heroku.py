3a07406ab896d596a92ab796bdaf6a54da0acd2a
Noah Balauro <noah@zenlogic.us>
2016-02-09 16:22:24 -0800

    Deplying to heroku

---
+++ cannasol_net/settings/heroku.py
@@ -0,0 +1,9 @@
+from .base import *
+
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
