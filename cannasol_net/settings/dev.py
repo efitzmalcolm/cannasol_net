5e3bf2e9eb2a501eb036495b27a24798d0cf55b3
Noah Balauro <noah@zenlogic.us>
2016-02-23 11:37:00 -0800

    Added .thumb class to wheretobuy thumbnails

--- cannasol_net/settings/dev.py
+++ cannasol_net/settings/dev.py
@@ -6,11 +6,10 @@ SECRET_KEY = 'e8033f7cd7'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cannasol.net',
+        'NAME': 'cannasol_net',
         'USER': 'dev',
         'PASSWORD': 'dev',
         'HOST': 'localhost',
         'PORT': '5432',
     }
 }
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
